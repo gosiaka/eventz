82d4b7c92a2dd92fc3e69ba6c62313228a857156
Gosia <malgorzata.kska@gmail.com>
2021-06-16 11:19:57 +0200

    Improve Events Index View

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,8 @@
 class Event < ApplicationRecord
+  # w modelu działamy na obiektach, zazwyczaj rekordach w bazie danych
+  # jesli metoda dotyczy jednego obiektu klasy (w całym Rubym) to jest instancyjna i tutaj używamy "self" wewnątrz metody
+
+  def free?
+    self.price.blank? || self.price.zero?
+  end
 end
