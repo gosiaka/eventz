06764316b0225411510d6daac0f923abf1853795
Gosia <malgorzata.kska@gmail.com>
2021-06-17 13:30:40 +0200

    Add Events Index links to layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  root "events#index"
   get "events" => "events#index"
   get "events/:id" => "events#show", as: "event" # dodaję helper - nazwę path przez: as:"event"
 end
