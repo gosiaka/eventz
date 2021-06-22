e8c48701ac86c6e10b8085fa39e141a816fcc43a
Gosia <malgorzata.kska@gmail.com>
2021-06-22 12:37:12 +0200

    Add validations to Event Model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,18 @@
 class Event < ApplicationRecord
   # w modelu działamy na obiektach, zazwyczaj rekordach w bazie danych
-  # jesli metoda dotyczy jednego obiektu klasy (w całym Rubym) to jest instancyjna i tutaj używamy "self" wewnątrz metody
+  # jesli metoda dotyczy jednego obiektu klasy (w całym Rubym) to jest instancyjna i tutaj używamy "self" wewnątrz metody(lub go pomijamy)
+  # jeśli jest klasowa (dotyczy wszystkich obiektów klasy) w jej nazwie używamy self.nazwa
+  
+  validates :name, :location, presence: true
+  validates :description, length: { minimum: 25 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validates :capacity, numericality: { only_integer: true, greater_than: 0 }
+  validates :image_file_name, format: { 
+    with: /\w+\.(jpg|png)\z/i,
+    message: "must be a JPG or PNG image"
+  }
 
+  
   def self.upcoming
     where("starts_at > ?", Time.now).order("starts_at")
   end
