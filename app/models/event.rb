99eaa36a1adcfab836efb731ee8b1c44f728a583
Gosia <malgorzata.kska@gmail.com>
2021-06-22 10:13:37 +0200

    Add upcoming method to Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,10 @@ class Event < ApplicationRecord
   # w modelu działamy na obiektach, zazwyczaj rekordach w bazie danych
   # jesli metoda dotyczy jednego obiektu klasy (w całym Rubym) to jest instancyjna i tutaj używamy "self" wewnątrz metody
 
+  def self.upcoming
+    where("starts_at > ?", Time.now).order("starts_at")
+  end
+  
   def free?
     self.price.blank? || self.price.zero?
   end
