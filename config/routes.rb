2b00a2a7ec3cac9264d2e07cf7f8d6f3ee29d49b
Gosia <malgorzata.kska@gmail.com>
2021-06-25 16:32:56 +0200

    Add User index, show, signup pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
 
   root "events#index"
 
+  resources :users, only: [:index, :show, :create]
+  get "signup" => "users#new"  # tworzymy custom route 'signup' dla akcji 'new' dla users
+
   resources :events do
-    resources :registrations, only: [:index, :new, :create ]
+    resources :registrations, only: [:index, :new, :create]
   end
 end
