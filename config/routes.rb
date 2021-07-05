f8baeae94b170f47bb45e3980c217e842b2a6225
Gosia <malgorzata.kska@gmail.com>
2021-07-05 10:42:21 +0200

    Add Edit Page to User (edit, update actions)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root "events#index"
 
-  resources :users, only: [:index, :show, :create]
+  resources :users, only: [:index, :show, :create, :edit, :update]
   get "signup" => "users#new"  # tworzymy custom route 'signup' dla akcji 'new' dla users
 
   resources :events do
