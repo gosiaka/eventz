b5f0734e094b989d192740f0561cdf673a5c6f3c
Gosia <malgorzata.kska@gmail.com>
2021-07-09 12:00:08 +0200

    Add Sessions controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Rails.application.routes.draw do
 
   root "events#index"
 
-  resources :users, only: [:index, :show, :create, :edit, :update, :destroy]
-  get "signup" => "users#new"  # tworzymy custom route 'signup' dla akcji 'new' dla users
-
   resources :events do
     resources :registrations, only: [:index, :new, :create]
   end
+
+  resources :users, only: [:index, :show, :create, :edit, :update, :destroy]
+  get "signup" => "users#new"  # tworzymy custom route 'signup' dla akcji 'new' dla users
+
+  resource :session, only: [:new, :create, :destroy] # dla jednej sesji - liczba pojedyncza
+  
+  
 end
