1b53530f857fd7c6a02bb9867b0f41f0d9e451c8
Gosia <malgorzata.kska@gmail.com>
2021-07-09 20:35:23 +0200

    Add Sign In feature in sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    params[:email]
-    params[:password]
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])      
+      session[:user_id] = user.id
+      redirect_to user, notice: "Welcome back #{user.name}!"
+    else
+      flash.now[:alert] = "Invalid email/password combitanion!"
+      render :new
+
+    end
   end
   
 end
