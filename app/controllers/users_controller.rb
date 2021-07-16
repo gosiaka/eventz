d8640eca7e51ef89a97fb7a06eaee119580aa601
Gosia <malgorzata.kska@gmail.com>
2021-07-16 10:56:52 +0200

    Add require_correct_user method and refactor UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   before_action :require_sign_in, except: [:new, :create]
+  before_action :require_correct_user, only: [:edit, :destroy, :update]
 
   def index
     @users = User.all
@@ -25,11 +26,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to @user, notice: "Account successfully updated"
     else
@@ -38,15 +37,21 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
-        @user.destroy
-        session[:user_id] = nil
-      redirect_to events_path, alert: "Account successfully deleted"
+    @user.destroy
+    session[:user_id] = nil
+    redirect_to events_path, alert: "Account successfully deleted"
   end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation) 
+  end
+
+  def require_correct_user
+    @user = User.find(params[:id])
+    unless current_user == @user
+      redirect_to events_path
+    end
   end
 end
