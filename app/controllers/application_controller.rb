dcf9b647fff3f8fc4a3afd7489b64b057c07fb4f
Gosia <malgorzata.kska@gmail.com>
2021-07-16 10:34:42 +0200

    Make current_user method helper avaliable in views and controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  
+  # avaliable for all controllers
+  def current_user
+    User.find(session[:user_id]) if session[:user_id]
+  end
+
+  # avaliable for all views
+  helper_method :current_user
+
 end
