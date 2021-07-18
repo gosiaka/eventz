a3b3f98be33dc24e4ae3c12c9dacbccf9a08a286
Gosia <malgorzata.kska@gmail.com>
2021-07-18 16:10:59 +0200

    Adjust Registration Views & controller to new features

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,7 @@
 class RegistrationsController < ApplicationController
 
+  before_action :require_sign_in
+
   # self.before_action(:set_event) - met. klasowa
   before_action :set_event # callback
 
@@ -13,6 +15,7 @@ class RegistrationsController < ApplicationController
 
   def create
     @registration = @event.registrations.new(registration_params)
+    @registration.user = current_user
 
     if @registration.save
       redirect_to event_registrations_path(@event), notice: "Thanks for registering!"
@@ -24,7 +27,7 @@ class RegistrationsController < ApplicationController
   private
 
   def registration_params
-    params.require(:registration).permit(:name, :email, :how_heard) # strong params
+    params.require(:registration).permit(:how_heard) # strong params
   end
 
   def set_event
